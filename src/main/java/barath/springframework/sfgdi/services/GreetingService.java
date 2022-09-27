fe57ff29b69e6d4c6793e87a872b7f83dc16d069
barath0211 <barathkumaarabirami@gmail.com>
2022-09-27 11:03:01 +0530

    Basics of Dependency Injection

---
+++ src/main/java/barath/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,6 @@
+package barath.springframework.sfgdi.services;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
---
+++ src/main/java/barath/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,8 @@
+package barath.springframework.sfgdi.services;
+
+public class GreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return null;
+    }
+}
