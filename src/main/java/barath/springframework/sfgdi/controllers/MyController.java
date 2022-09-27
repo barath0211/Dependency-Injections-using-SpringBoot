d4add19bc5c4ab8786ed7294df69fcaaea622c1a
barath0211 <barathkumaarabirami@gmail.com>
2022-09-27 14:43:14 +0530

    Using Primary Beans

--- src/main/java/barath/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/barath/springframework/sfgdi/controllers/MyController.java
@@ -1,14 +1,17 @@
 package barath.springframework.sfgdi.controllers;
 
+import barath.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String sayHello() {
-
-        System.out.println("Hello World");
-
-        return "Hi Folks!";
+return greetingService.sayGreeting();
     }
 }
