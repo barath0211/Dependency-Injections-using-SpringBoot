4c610309bd6416d87e1613e451ae73cd2f904438
barath0211 <barathkumaarabirami@gmail.com>
2022-09-28 14:32:54 +0530

    Spring Default Profile

--- src/main/java/barath/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/barath/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -2,7 +2,7 @@ package barath.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("En")
+@Profile({"En","default"})
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService{
     @Override
