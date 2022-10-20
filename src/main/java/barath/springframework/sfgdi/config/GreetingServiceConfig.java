d050b1837edf8fa8f83c3d993d27068a0733efb3
barath0211 <barathkumaarabirami@gmail.com>
2022-10-20 07:32:20 +0530

    Usage of Factory Beans

--- src/main/java/barath/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/barath/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -3,6 +3,8 @@ package barath.springframework.sfgdi.config;
 import barath.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import barath.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import barath.springframework.sfgdi.services.*;
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -10,6 +12,21 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+      return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile({"cat"})
+    @Bean
+    PetService catPetService (PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
 
     @Profile({"ES", "default"})
     @Bean("i18nService")
